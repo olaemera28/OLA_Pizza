3cc31fdfe298e3cf4da6b239af334b325852c1be
olaemera28 <132638066+olaemera28@users.noreply.github.com>
2023-05-05 08:03:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function photo(x){
+        img.src = x;
+}
+
+function colors(color){
+         container.style.background = color; 
+
+}
+
